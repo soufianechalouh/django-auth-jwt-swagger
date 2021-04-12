b1c6c76e39407b6fdf9b4298ce7e8e517413fae0
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-04-12 23:18:20 +0000

    Create JWT auth class and add it to rest framework's default auth

--- poc_django_auth_jwt/settings.py
+++ poc_django_auth_jwt/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'authentication'
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "authentication.backends.JWTAuthentication"
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -126,3 +132,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# JWT
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "key")
